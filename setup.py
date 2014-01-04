71818e3f4e4d21b584bc7d85103398ce7456658b
Yan Kalchevskiy <yan.kalchevskiy@gmail.com>
2014-01-04 19:48:28 +0700

    Added package information.

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+from habra_favorites import get_version
+
+
+def get_long_description():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='habra-favorites',
+
+    author='Yan Kalchevskiy',
+    author_email='yan.kalchevskiy@gmail.ru',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Utilities',
+    ],
+    description='Sort your favorites posts from Habrahabr.ru',
+    include_package_data=True,
+    keywords=['scrapy', 'habrahabr.ru'],
+    license='MIT',
+    long_description=get_long_description(),
+    packages=find_packages(),
+    package_data={
+        '': ['*.html'],
+    },
+    url='https://github.com/ykalchevskiy/habra-favorites',
+    version=get_version(),
+
+    entry_points={
+        'console_scripts': ['habra_favorites = habra_favorites.main:main']
+    },
+    install_requires=['Scrapy==0.20.2']
+)

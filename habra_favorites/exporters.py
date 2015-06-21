94c8533c04dce353f9d904c49347564653704f33
Yan Kalchevksiy <yan.kalchevskiy@gmail.com>
2015-06-21 23:12:16 +0300

    Removed magic number.

--- habra_favorites/exporters.py
+++ habra_favorites/exporters.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 import os
 
 from scrapy.exporters import JsonItemExporter
@@ -10,7 +9,7 @@ class HtmlItemExporter(JsonItemExporter):
 
     def __init__(self, file_, **kwargs):
         super(HtmlItemExporter, self).__init__(file_, **kwargs)
-        self.file.seek(0)
+        self.file.seek(os.SEEK_SET)
         self.file.truncate()
         with open(os.path.join(os.path.dirname(__file__), 'templates', self.TEMPLATE)) as f:
             self.file_start, self.file_finish = f.read().split('// ITEMS')
